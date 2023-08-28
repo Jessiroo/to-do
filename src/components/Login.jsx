91f7ffd967968fe7a5a57f0ad2fc32b8e1203b51
Jessiroo <jessie.madsen@gmail.com>
2023-08-27 20:31:10 -0600

    refining login/register functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,15 +4,21 @@ import Card from '../Layout/Card';
 import Button from '../Layout/Button';
 import classes from './Login.module.css';
 import AuthContext from '../context/auth-context';
+import { set } from 'firebase/database';
 
 const Login = () => {
   const { updateUser, createUser } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [verifyPassword, setVerifyPassword] = useState('');
+  // const [verifyPassword, setVerifyPassword] = useState('');
   const [isRegistering, setIsRegistering] = useState(false);
+  const [registerInfo, setRegisterInfo] = useState({
+    email: "",
+    password: "",
+    verifyPassword: "",
+  });
 
-  // Handler Functions
+  // Sign In Handlers
   const emailChangeHandler = (event) => {
     setEmail(event.target.value);
   };
@@ -21,20 +27,45 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
-  const verifyPasswordHandler = (event) => {
-    setVerifyPassword(event.target.value);
-  };
+  // const verifyPasswordHandler = (event) => {
+  //   setVerifyPassword(event.target.value);
+  // };
 
   const signInHandler = () => {
     updateUser(email, password);
   };
 
+  // Create Account Handlers
   const createAccountHandler = () => {
     setIsRegistering(true);
   };
 
-  const registerHandler = () => {
-    createUser(email, password, verifyPassword);
+  const registerEmailChangeHandler = (event) => {
+    setRegisterInfo({
+      email: event.target.value,
+      password: registerInfo.password,
+      verifyPassword: registerInfo.verifyPassword,
+    });
+  };
+
+  const registerPasswordChangeHandler = (event) => {
+    setRegisterInfo({
+      email: registerInfo.email,
+      password: event.target.value,
+      verifyPassword: registerInfo.verifyPassword,
+    });
+  };
+
+  const registerVerifyPasswordHandler = (event) => {
+    setRegisterInfo({
+      email: registerInfo.email,
+      password: registerInfo.password,
+      verifyPassword: event.target.value,
+    });
+  };
+
+  const registerUserHandler = () => {
+    createUser(registerInfo.email, registerInfo.password, registerInfo.verifyPassword);
   };
 
   const cancelRegisterHandler = () => {
@@ -48,11 +79,13 @@ const Login = () => {
         onChange={emailChangeHandler} 
         type="email" 
         placeholder="Email"
+        value={email}
       />
       <input 
         onChange={passwordChangeHandler} 
         type="password" 
         placeholder="Password"
+        value={password}
       />
       <Button onClick={signInHandler}>Submit</Button>
       <Button onClick={createAccountHandler}>Create an Account</Button>
@@ -64,21 +97,24 @@ const Login = () => {
     content = (
       <>
         <input 
-          onChange={emailChangeHandler} 
+          onChange={registerEmailChangeHandler} 
           type="email" 
           placeholder="Email" 
+          value={registerInfo.email}
         />
         <input 
-          onChange={passwordChangeHandler} 
+          onChange={registerPasswordChangeHandler} 
           type="password" 
           placeholder="Password" 
+          value={registerInfo.password}
         />
         <input 
-          onChange={verifyPasswordHandler} 
+          onChange={registerVerifyPasswordHandler} 
           type="password" 
           placeholder="Confirm Password" 
+          value={registerInfo.verifyPassword}
         />
-        <Button onClick={registerHandler}>Register</Button>
+        <Button onClick={registerUserHandler}>Register</Button>
         <Button onClick={cancelRegisterHandler}>Cancel</Button>
       </>
     );
